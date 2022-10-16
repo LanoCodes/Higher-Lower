630f77957e7a377f4f3469e5ce253da17d9c6f6f
LanoCodes <del.pow1@gmail.com>
2022-10-16 13:42:06 -0400

    some cleanup work in main

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import random as r
 
 
 
-# i think there is a way to make this solution recursive. I want to choose that option because I think it might be a very straightforward way of persistent the correctly chose higher insta acct towards the next comparison
 def InstaAcctGen():
-  """Returns a random istagram acount that has it's own data in the form of a dictionary."""
+  """Returns a random istagram acount that has its own data in the form of a dictionary."""
   return data[r.randint(0, len(data) - 1)]
 
 def HigherOrLower(acct1, acct2, score):
   """Compares instagram accounts for the highest follower count"""
-  # print out the program logo
   print(logo)
   if score != 0:
     print(f"You got it right! Current Score: {score}")
@@ -25,7 +23,6 @@ def HigherOrLower(acct1, acct2, score):
   user_account_choice = input("Which account has more followers? Type 'A', or 'B': ")
   # if the user inputted 'A' then their choice has to tracked to acct1, if input was 'B', then their choice has to be tracked to acct2
   if user_account_choice == 'A':
-    # user_account_choice = acct1['follower_count']
     user_account_choice = acct1
     account_followers_compared_to = acct2['follower_count']
   else:
@@ -46,13 +43,4 @@ def HigherOrLower(acct1, acct2, score):
 score = 0
 
 HigherOrLower(InstaAcctGen(), InstaAcctGen(), score)
-  
-# a while loop that keeps repeating until winning flag is False
-# asking the user to pick between choice A or B, maybe some error handling there for instances of lowercase inputs
-# Sentence formatting of the data for the choices that makes sense.
-# a patterned way to get the same sentence structure from eawc
-# so flag that tracks that the user is winning still
-# a vs logo between the two choices
-#  a point tracker 
-# text to display points when user loses
 
